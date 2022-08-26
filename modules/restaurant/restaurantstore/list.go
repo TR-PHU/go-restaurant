14a87debf12215a8b2b94244099329898b6b215c
Thanh Phu <tranphutptv@gmail.com>
2022-08-26 19:53:47 +0700

    add seeking method for paging

--- modules/restaurant/restaurantstore/list.go
+++ modules/restaurant/restaurantstore/list.go
@@ -31,12 +31,29 @@ func (s *sqlStore) ListDataByCondition(ctx context.Context,
 		return nil, common.ErrDB(err)
 	}
 
+	if v := paging.FakeCursor; v != "" {
+		uid, err := common.FromBase58(v)
+
+		if err != nil {
+			return nil, common.ErrDB(err)
+		}
+		db = db.Where("id < ?", uid.GetLocalID())
+	} else {
+		offset := (paging.Page - 1) * paging.Limit
+		db = db.Offset(offset)
+	}
+
 	if err := db.
 		Order("id desc").
 		Limit(paging.Limit).
-		Offset((paging.Page - 1) * paging.Limit).
 		Find(&result).Error; err != nil {
 		return nil, common.ErrDB(err)
+	}
+
+	if len(result) > 0 {
+		last := result[len(result)-1]
+		last.Mask(false)
+		paging.NextCursor = last.FakeId.String()
 	}
 
 	return result, nil
